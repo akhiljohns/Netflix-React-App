dba18bec9a93a02dd63c4ca5bea95232c4a15a41
AKHIL JOHNS <akhiljohns250@gmail.com>
2023-09-28 14:42:32 +0530

    Updated env

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,6 +1,7 @@
 
-export const baseUrl = process.env.REACT_APP_TMDB_BASE_URL;
-export const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
-export const imageUrl = process.env.REACT_APP_TMDB_IMAGE_URL;
+export const baseUrl = process.env.REACT_APP_TMDB_BASE_URL
+export const API_KEY = process.env.REACT_APP_TMDB_API_KEY
+export const imageUrl = process.env.REACT_APP_TMDB_IMAGE_URL
 
-// https://api.themoviedb.org/3/discover/tv?api_key={API_KEY_HERE}&with_networks=213
+// https://api.themoviedb.org/3/discover/tv
+// ?api_key={API_KEY_HERE}&with_networks=213
