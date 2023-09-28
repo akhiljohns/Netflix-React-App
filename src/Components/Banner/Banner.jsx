c9f6ef223d14dcd742ca5fb176e058712b24afbb
AKHIL JOHNS <akhiljohns250@gmail.com>
2023-09-28 15:15:18 +0530

    Banner Loading Bug Fixed

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,24 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./Banner.css";
 import axios from '../../axios'
-import {API_KEY} from '../../Constants/Constants'
-import {imageUrl} from '../../Constants/Constants'
+import { API_KEY } from '../../Constants/Constants'
+import { imageUrl } from '../../Constants/Constants'
 
 function Banner() {
   const min = 0;
   const max = 20;
-  const ranNum = Math.floor(Math.random() * (max - min + 1)) + min;
-  
+  const ranNumRef = useRef(); // Use a ref to store ranNum
+
   const [movie, setMovie] = useState();
 
   useEffect(() => {
+    // Generate a new random number if it's not already set
+    if (!ranNumRef.current) {
+      ranNumRef.current = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
     axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response) => {
-      setMovie(response.data.results[ranNum]);
+      setMovie(response.data.results[ranNumRef.current]);
     });
-  }, [ranNum]); // Include ranNum in the dependency array
+  }, []); // No dependencies here
 
   return (
-    <div style={{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ' '})`}} className="banner">
+    <div style={{ backgroundImage: `url(${movie ? imageUrl + movie.backdrop_path : ' '})` }} className="banner">
       <div className="content">
         <h1 className="title">
           {movie && (movie.title || movie.name) ? movie.title || movie.name : ''}
